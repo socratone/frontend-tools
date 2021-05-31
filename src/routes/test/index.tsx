578c1e9dd389ae1b8700139b8e4662518c3cdde7
socratone <gim2origin@gmail.com>
2021-05-31 23:33:56 +0900

    feat: test passed 표시

--- src/routes/test/index.tsx
+++ src/routes/test/index.tsx
@@ -1,17 +1,45 @@
 import { Link } from 'react-router-dom';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 import Column from '../../components/box/Column';
+import { Item } from './styles';
 import datas from '../testId/datas';
+import { useEffect, useState } from 'react';
 
 const Test = () => {
+  const [passeds, setPasseds] = useState<{[key: string]: boolean | undefined}>({})
+
+  useEffect(() => {
+    const passedsString = localStorage.getItem('passeds');
+    if (!passedsString) {
+      localStorage.setItem('passeds', '{}');
+    } else {
+      const passedsObject = JSON.parse(passedsString);
+      setPasseds(passedsObject);
+    }
+  }, []);
+
   return (
     <div>
-      <Column gap={10} mt="10px">
-        {datas.map(data => {
-          return (
-            <li key={data.id}>
-              <Link to={`/test/${data.id}`}>{data.question}</Link>
-            </li>
-          )
+      <Column mt="10px">
+        {datas.map((data) => {
+          if (passeds[data.id]) {
+            return (
+              <Item key={data.id}>
+                <CheckBoxIcon style={{ fontSize: '20px', color: 'green' }} />
+                <Link to={`/test/${data.id}`}>{data.question}</Link>
+              </Item>
+            );
+          } else {
+            return (
+              <Item key={data.id}>
+                <CheckBoxOutlineBlankIcon
+                  style={{ fontSize: '20px', color: 'dimgrey' }}
+                />
+                <Link to={`/test/${data.id}`}>{data.question}</Link>
+              </Item>
+            );
+          }
         })}
       </Column>
     </div>
---
+++ src/routes/test/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Item = styled.li`
+  display: flex;
+  align-items: center;
+
+  & a {
+    color: black;
+  }
+`;
